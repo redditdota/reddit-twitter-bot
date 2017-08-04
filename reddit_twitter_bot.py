4425a1ef72224ace2eda6ca469373384e9defe8e
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2017-08-04 01:25:55 -0400

    capitalize ti

--- reddit_twitter_bot.py
+++ reddit_twitter_bot.py
@@ -153,7 +153,7 @@ def process_title(post):
     is_esports = "esports" in post["flair"].lower()
     hashtag = HASHTAG
     if is_esports:
-        hashtag = HASHTAG + " #ti7"
+        hashtag = HASHTAG + " #TI7"
 
     max_length = 139 - 3
     if post["url"]:
